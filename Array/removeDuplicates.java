01c9b16f720910be5d321f53358c83ba16122eb9
Akilan Selvam <54879150+akilanselvam@users.noreply.github.com>
2023-08-07 23:31:23 +0530

    07-08-2023 removeDuplicates.java

--- Array/removeDuplicates.java
+++ Array/removeDuplicates.java
@@ -7,7 +7,7 @@ class Solution {
         
         int k = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
+            if (nums[i] != nums[i - 1]) { 
                 nums[k] = nums[i];
                 k++;
             }
@@ -16,3 +16,5 @@ class Solution {
         return k;
     }
 }
+//initiating with second value considering the first value to be unique and comparing it with previous value to check they are unique or not
+//if not unique then we are overwriting with the help of k
