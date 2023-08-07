e9752d733d64bfe5999a1a858bf11950570c8d19
Akilan Selvam <54879150+akilanselvam@users.noreply.github.com>
2023-08-07 23:28:47 +0530

    07-08-2023 removeDuplicates.java

---
+++ Array/removeDuplicates.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+   
+       if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        
+        return k;
+    }
+}
